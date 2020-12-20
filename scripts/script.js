c9988601ef00901cffdc0f8af1cd7ce106ec5e4f
Stefan Rogovskiy <panst3pan@yandex.ru>
2020-12-20 17:44:42 +0700

    bugs fixed #1

--- scripts/script.js
+++ scripts/script.js
@@ -2,36 +2,39 @@
 const popup = document.querySelector('.popup');
 const editBtn = document.querySelector('.profile__edit-button');
 const formElement = document.querySelector('.popup__container');
+
+//заполнение текстовых полей
 const popupField = document.querySelectorAll('.popup__field');
 const profileName = document.querySelector('.profile__name');
 const profileDesc = document.querySelector('.profile__description');
+
+const inputName = document.querySelector('.popup__field_name');
+const inputDesc = document.querySelector('.popup__field_description');
+
 const сlosePopupBtn = document.querySelector('.popup__close-button');
 
 
 function openPopup() {
   popup.classList.add('popup_opened');
+  inputName.value = profileName.textContent;
+  inputDesc.value = profileDesc.textContent;
 }
-editBtn.addEventListener('click', openPopup);
-
-
-popupField[0].value = profileName.textContent;
-popupField[1].value = profileDesc.textContent;
 
 
 function closePopup() {
     popup.classList.remove('popup_opened');
 }
-сlosePopupBtn.addEventListener('click', closePopup);
 
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
 
-  let nameInput = popupField[0].value;
-  profileName.textContent = nameInput;
-  let descInput = popupField[1].value;
-  profileDesc.textContent = descInput;
+  profileName.textContent = inputName.value;
+  profileDesc.textContent = inputDesc.value;
 
   closePopup();
 }
+
+editBtn.addEventListener('click', openPopup);
+сlosePopupBtn.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
