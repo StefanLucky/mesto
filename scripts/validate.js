60a3d8ab4534ea72300702bc242a914fd9241208
Stefan Rogovskiy <panst3pan@yandex.ru>
2021-01-01 22:32:18 +0300

    Sprint 6.1.2 tests fixed

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,8 +1,8 @@
 const validationConfig = {
     formSelector: '.popup__form',
     inputSelector: '.popup__input',
-    submitButtonSelector: '.popup__button',
-    inactiveButtonClass: 'popup__button_disabled',
+    submitBtnSelector: '.popup__button',
+    inactiveBtnClass: 'popup__button_disabled',
     inputErrorClass: 'popup__input_type_error',
     errorClass: 'popup__error_visible'
 }
@@ -35,31 +35,31 @@ function checkInputValidity(form, input, config) {
 }
 
 // Блокируем кнопки
-function setButtonState(button, isActive, config) {
+function setBtnState(button, isActive, config) {
     if (isActive) {
-        button.classList.remove(config.inactiveButtonClass);
+        button.classList.remove(config.inactiveBtnClass);
         button.disabled = false;
     } else {
-        button.classList.add(config.inactiveButtonClass);
+        button.classList.add(config.inactiveBtnClass);
         button.classList.remove('button');
         button.disabled = true;
     }
 }
 
-// Добавляем слушателей на инпутах и проверяем валидность
+// Добавляем слушателей/проверяем валидность
 function setEventListener(form, config) {
     const inputList = form.querySelectorAll(config.inputSelector);
-    const submitButton = form.querySelector(config.submitButtonSelector);
+    const submitButton = form.querySelector(config.submitBtnSelector);
 
     inputList.forEach(input => {
         input.addEventListener('input', (evt) => {
             checkInputValidity(form, input, config);
-            setButtonState(submitButton, form.checkValidity(), config);
+            setBtnState(submitButton, form.checkValidity(), config);
         });
     });
 }
 
-// Включаем валидацию
+// 
 function enableValidation(config) {
     const forms = document.querySelectorAll(config.formSelector);
     forms.forEach(form => {
